2b647274095e5cd1ad5c11d2c952b208056d8380
arseniymusorin@gmail.com <quaka@quaka.localdomain>
2026-05-25 23:13:02 +0300

    add item.hpp

---
+++ musorin.arseniy/F0/item.hpp
@@ -0,0 +1,38 @@
+#ifndef ITEM_HPP
+#define ITEM_HPP
+
+#include <string>
+#include <vector>
+
+enum class ItemType
+{
+  Material,
+  Weapon,
+  Armor,
+  Food,
+  Other
+};
+
+enum class SlotType
+{
+  Weapon,
+  Head,
+  Chest,
+  Legs,
+  Feet,
+  Offhand,
+  None
+};
+
+struct Item
+{
+  std::string id;
+  std::string name;
+  ItemType type;
+  int durability;
+  int maxStack;
+  SlotType slot;
+  double power;
+};
+
+#endif
